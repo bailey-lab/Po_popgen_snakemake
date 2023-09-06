fa43a4ba15a2ddc2b100404c96ea194ae5dd819c
Kelly Carey-Ewend <kcareyewend@gmail.com>
2023-09-06 12:28:26 -0400

    combined snakefile scripts into single pipeline directory titled 'full'

--- full/workflow/scripts/submit_snakefile.sh
+++ full/workflow/scripts/submit_snakefile.sh
@@ -3,6 +3,6 @@ module load anaconda/2021.11
 #conda create -c conda-forge -c bioconda -n snakemake snakemake
 conda activate snakemake
 cp workflow/Snakefile_processing.py workflow/Snakefile_submission.py
-snakemake --use-conda -s workflow/Snakefile_submission.py --profile slurm
+snakemake --use-conda -s workflow/Snakefile_submission.py --profile slurm --rerun-incomplete
 ### --profile slurm command tells snakemake to check current directory for a folder caller "slurm" and then for a config.yaml file within
-snakemake --use-conda -s workflow/Snakefile_ortholog_prep.py --profile slurm
+snakemake --use-conda -s workflow/Snakefile_ortholog_prep.py --profile slurm --rerun-incomplete
